2b1f20a2af38930b2a65aef950bdb1aaa7d8d6ae
Pedro Pozo <pedroantoniopozo2003@gmail.com>
2024-04-16 01:01:25 +0200

    Modelo Anuncio.

--- app/src/main/java/com/dam/proteccioncivil/data/model/Anuncio.kt
+++ app/src/main/java/com/dam/proteccioncivil/data/model/Anuncio.kt
@@ -1,12 +1,12 @@
 package com.dam.proteccioncivil.data.model
 
-import org.jetbrains.exposed.sql.Table;
-import org.jetbrains.exposed.sql.Column;
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.date
 import java.time.LocalDate
 
 object Anuncio : Table() {
     val fecha: Column<LocalDate> = date("fecha")
-    val id: Column<Int> = integer("ID").autoIncrement().primaryKey()
     val texto: Column<String> = text("texto")
+    override val primaryKey = PrimaryKey(fecha, name = "ID")
 }
