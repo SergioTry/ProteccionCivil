e13a8fdbdee2a24a42e3b2ff12c9a65bd93a65d8
Pedro Pozo <pedroantoniopozo2003@gmail.com>
2024-04-22 21:03:32 +0200

    Creation of Splash and Login screens and a new logo has been added.

---
+++ app/src/main/java/com/dam/proteccioncivil/ui/screens/login/LoginScreen.kt
@@ -0,0 +1,85 @@
+package com.dam.proteccioncivil.ui.screens.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dam.proteccioncivil.R
+
+@Composable
+fun SplashScreen(version: String) {
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "SplashScreen image",
+                modifier = Modifier
+                    .size(200.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(
+                text = "¡Bienvenido!",
+                modifier = Modifier.padding(8.dp)
+            )
+            TextField(
+                value = "Su identificador",
+                onValueChange = {},
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            Text(
+                text = "¡Bienvenido!",
+                modifier = Modifier.padding(8.dp)
+            )
+            TextField(
+                value = "Su clave", onValueChange = {},
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            Text(
+                text = "¡Bienvenido!",
+                modifier = Modifier.padding(top = 16.dp).align(Alignment.CenterHorizontally)
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(
+                text = "Versión: $version",
+                modifier = Modifier.padding(16.dp),
+                textAlign = androidx.compose.ui.text.style.TextAlign.End
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true, device = Devices.PIXEL_5)
+@Composable
+fun PreviewSplashScreen() {
+    SplashScreen(version = "0.0.1")
+}
